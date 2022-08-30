7f444dc1d4fc70c5d73a715f25a73a97b7e466f1
Nikita <n.d.2002z@gmail.com>
2022-08-30 17:50:48 +0300

    format

--- src/features/auth/auth.provider.tsx
+++ src/features/auth/auth.provider.tsx
@@ -1,105 +1,102 @@
-import {useState, useEffect, useContext} from 'react';
-import {AuthContext} from './auth.context';
-import {API} from '../../infrastructure/api';
-import {User} from './auth.entity';
-import {useNavigate} from "react-router-dom";
+import { useState, useEffect, useContext } from 'react';
+import { AuthContext } from './auth.context';
+import { API } from '../../infrastructure/api';
+import { User } from './auth.entity';
+import { useNavigate } from 'react-router-dom';
 
 enum HTTP_RES {
-    "SUCCESS" = 200,
-    "FORBIDDEN" = 403
+  'SUCCESS' = 200,
+  'FORBIDDEN' = 403,
 }
 
-
 export const AuthProvider = (props: any) => {
-    const [isAuth, setIsAuth] = useState<boolean>(false);
-    const [user, setUser] = useState<User | undefined>(undefined);
-    const navigate = useNavigate()
-    const {children} = props;
+  const [isAuth, setIsAuth] = useState<boolean>(false);
+  const [user, setUser] = useState<User | undefined>(undefined);
+  const navigate = useNavigate();
+  const { children } = props;
 
-    useEffect(() => {
-        console.log("ASASSASAAS")
-        fetch(API.PROFILE_URL)
-            .then((res) => {
-                if (res.status === HTTP_RES.SUCCESS) {
-                    setIsAuth(true);
-                    res.json().then((res) => {
-                        const newUser: User = {
-                            name: res.data.name,
-                        };
-                        setUser(newUser);
-                        navigate("/login")
-                    });
-                } else {
-                    setIsAuth(false);
-                }
-            })
+  useEffect(() => {
+    console.log('ASASSASAAS');
+    fetch(API.PROFILE_URL).then((res) => {
+      if (res.status === HTTP_RES.SUCCESS) {
+        setIsAuth(true);
+        res.json().then((res) => {
+          const newUser: User = {
+            name: res.data.name,
+          };
+          setUser(newUser);
+          navigate('/login');
+        });
+      } else {
+        setIsAuth(false);
+      }
+    });
+  }, []);
 
-    }, []);
+  const login = (email: string, phone: string, password: string) => {
+    const req = {
+      email,
+      phone,
+      password,
+    };
+    fetch(API.LOGIN_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify(req),
+    }).then((res) => {
+      console.log('REQ: ', JSON.stringify(req));
+      console.log('RES: ', res);
+      if (res.status === HTTP_RES.SUCCESS) {
+        setIsAuth(true);
+        getProfile();
+      } else {
+        setIsAuth(false);
+      }
+    });
+  };
 
-    const login = (email: string, phone: string, password: string) => {
-        const req = {
-            email,
-            phone,
-            password,
+  const logout = () => {
+    fetch(API.LOGOUT_URL, {
+      method: 'POST',
+    }).then((res) => {
+      console.log('RES: ', res);
+      if (res.status === HTTP_RES.SUCCESS) {
+        setIsAuth(false);
+      }
+    });
+  };
+
+  const getProfile = () => {
+    fetch(API.PROFILE_URL)
+      .then(async (res) => await res.json())
+      .then((res) => {
+        const newUser: User = {
+          name: res.data.name,
         };
-        fetch(API.LOGIN_URL, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8',
-            },
-            body: JSON.stringify(req),
-        }).then((res) => {
-            console.log('REQ: ', JSON.stringify(req));
-            console.log('RES: ', res);
-            if (res.status === HTTP_RES.SUCCESS) {
-                setIsAuth(true);
-                getProfile();
-            } else {
-                setIsAuth(false);
-            }
-        });
-    };
+        setUser(newUser);
+      });
+  };
 
-    const logout = () => {
-        fetch(API.LOGOUT_URL, {
-            method: 'POST',
-        }).then((res) => {
-            console.log('RES: ', res);
-            if (res.status === HTTP_RES.SUCCESS) {
-                setIsAuth(false);
-            }
-        });
-    };
-
-    const getProfile = () => {
-        fetch(API.PROFILE_URL)
-            .then(async (res) => await res.json())
-            .then((res) => {
-                const newUser: User = {
-                    name: res.data.name,
-                };
-                setUser(newUser);
-            });
-    };
-
-    return (
-        <AuthContext.Provider
-            value={{
-                user,
-                isAuth,
-                login,
-                logout,
-            }}
-        >
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        isAuth,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const useAuthContext = () => {
-    const value = useContext(AuthContext);
-    if (value == null) {
-        throw Error('useAuthContext can only be used inside AuthProvider');
-    }
-    return value;
+  const value = useContext(AuthContext);
+  if (value == null) {
+    throw Error('useAuthContext can only be used inside AuthProvider');
+  }
+  return value;
 };
--- src/screens/LoginScreen/LoginScreen.tsx
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -1,14 +1,14 @@
-import {LoginForm} from '../../features/auth';
-import {DefaultLayout} from '../../features/layouts/DefaultLayout';
-import {FC} from "react";
+import { LoginForm } from '../../features/auth';
+import { DefaultLayout } from '../../features/layouts/DefaultLayout';
+import { FC } from 'react';
 import styles from './LoginScreen.module.css';
 
 export const LoginScreen: FC = () => {
-    return (
-        <DefaultLayout>
-            <div className={styles.login_form__container}>
-                <LoginForm/>
-            </div>
-        </DefaultLayout>
-    );
+  return (
+    <DefaultLayout>
+      <div className={styles.login_form__container}>
+        <LoginForm />
+      </div>
+    </DefaultLayout>
+  );
 };
